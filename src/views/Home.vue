febec63d65c48ea384a622a98a61dd2e433d1422
Jesray Garciano <jesray.garciano@gmail.com>
2018-12-07 11:00:41 +0800

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <Dashboard msg="Welcome to Dashboard" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Dashboard from '@/components/Dashboard.vue'
 
 @Component({
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Dashboard
   },
 })
 export default class Home extends Vue {}
